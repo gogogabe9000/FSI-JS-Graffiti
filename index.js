93dccc782579855cd4f5faf20265eddd58d2cb86
gogogabe9000 <gabe.email1032@yahoo.com>
2022-01-26 19:22:36 -0500

    Debugging and trying to alig n dog names left

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ let dogImages = document.querySelectorAll('.dog-image')
 for(let i=0; i < dogImages.length; i++){
     dogImages[i].style.borderRadius = '50px'
 }
+
+let dogName = document.querySelectorAll('.dog-Name')
+for(let i=0: i < dogName.length; i++){
+dogName[i].header.style.textAlign = 'left'
+console.log(dogName)
+}
