e4eb984731d461d497f9c8342c2c797cf21461ba
gogogabe9000 <gabe.email1032@yahoo.com>
2022-01-29 11:01:06 -0500

    Flipped images upside down, finalized the project

--- index.js
+++ index.js
@@ -3,9 +3,10 @@
 let header = document.querySelector('#page-header') //aligns the header titled "doggos" to the left
 header.style.textAlign = 'left'
 
-let dogImages = document.querySelectorAll('.dog-image') //changes the borders of the dog images to be rounded
+let dogImages = document.querySelectorAll('.dog-image') //changes the borders of the dog images to be rounded as well as flip the images upside down
 for(let i=0; i < dogImages.length; i++){
-    dogImages[i].style.borderRadius = '50px'
+    dogImages[i].style.borderRadius = '50px';
+    dogImages[i].style.transform = 'rotate(180deg)' 
 }
 
 let dogName = document.querySelectorAll('.dog-name') //Aligns the names of all the dogs to the left.
@@ -13,7 +14,7 @@ for(let i=0; i < dogName.length; i++){
     dogName[i].style.textAlign = 'left'
 }
 
-let footerColor = document.querySelectorAll('.dog-description')
+let footerColor = document.querySelectorAll('.dog-description') //Changes the color of the footer text to blue.
 for(let i=0; i < dogName.length; i++){
     footerColor[i].style.color = 'blue'
 }
