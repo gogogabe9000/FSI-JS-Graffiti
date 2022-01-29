d19383e48b9e87929a0305bfb094590db7325da2
gogogabe9000 <gabe.email1032@yahoo.com>
2022-01-29 10:13:56 -0500

    debugged starter code, however dog names are not aligned left

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 
 // Select some elements...
-let header = document.querySelector('#page-header')
+let header = document.querySelector('#page-header') //aligns the header titled "doggos" to the left
 header.style.textAlign = 'left'
 
-let dogImages = document.querySelectorAll('.dog-image')
+let dogImages = document.querySelectorAll('.dog-image') //changes the borders of the dog images to be rounded
 for(let i=0; i < dogImages.length; i++){
     dogImages[i].style.borderRadius = '50px'
 }
 
-let dogName = document.querySelectorAll('.dog-Name')
-for(let i=0: i < dogName.length; i++){
-dogName[i].header.style.textAlign = 'left'
-console.log(dogName)
+let dogName = document.querySelectorAll('.dog-name')
+for(let i=0; i < dogName.length; i++){
+    dogname[i].style.textAlign = 'left'
 }
